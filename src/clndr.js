a5704c8fd4169ee18772455e86c09448259f126d
kumori <kumorig@gmail.com>
2018-05-19 13:29:12 +0900

    error message fixed

--- src/clndr.js
+++ src/clndr.js
@@ -1520,20 +1520,23 @@ function isElement(obj) {
     //Using W3 DOM2 (works for FF, Opera and Chrome)
     return obj instanceof HTMLElement;
   }
-  catch(e){
+  catch (e) {
     //Browsers not supporting W3 DOM2 don't have HTMLElement and
     //an exception is thrown and we end up here. Testing some
     //properties that all elements have (works on IE7)
-    return (typeof obj==="object") &&
-      (obj.nodeType===1) && (typeof obj.style === "object") &&
-      (typeof obj.ownerDocument ==="object");
+    return (typeof obj === 'object') &&
+      (obj.nodeType === 1) && (typeof obj.style === 'object') &&
+      (typeof obj.ownerDocument === 'object');
   }
 }
 
 export function createClndr(elementOrSelector, options) {
-  let elem =  isElement(elementOrSelector) ? elementOrSelector : document.querySelector(elementOrSelector);
+  let elem = isElement(elementOrSelector) ? elementOrSelector : document.querySelector(elementOrSelector);
   if (!elem) {
-    throw new Error('First argument needs to be an element or a selector that matches at least one element (the first match will be used)');
+    throw new Error(
+      `First argument needs to be an element or a selector  matching at least one element (where first match will be used). 
+      \nThe passed in value is: <${typeof elementOrSelector}> ${elementOrSelector}`
+    );
   }
   return new Clndr(elem, options);
 }
